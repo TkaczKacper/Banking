9cb46a18fc9ec1e5217623eb9934d5b5da524d09
TkaczKacper <kacpertk2016@gmail.com>
2023-01-15 00:18:18 +0100

    testing

--- testing/src/__tests__/useBreedList.test.jsx
+++ testing/src/__tests__/useBreedList.test.jsx
@@ -1,5 +1,5 @@
 import { expect, test } from "vitest";
-import { render, renderHook } from "@testing-library/react";
+import { render, renderHook, waitFor } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import useBreedList from "../useBreedList";
 
@@ -47,4 +47,36 @@ test("gives an empty list", async () => {
      const [breedList, status] = result.current;
      expect(breedList).toHaveLength(0);
      expect(status).toBe("loading");
+});
+
+test("gives back breeds with an animal", async () => {
+     const breeds = [
+          "Havanese",
+          "Bichon Frise",
+          "Poodle",
+          "Maltese",
+          "Golden Retriever",
+          "Labrador",
+          "Husky",
+     ];
+
+     fetch.mockResponseOnce(
+          JSON.stringify({
+               animal: "dog",
+               breeds,
+          })
+     );
+
+     const { result } = renderHook(() => useBreedList("dog"), {
+          wrapper: ({ children }) => (
+               <QueryClientProvider client={queryClient}>
+                    {children}
+               </QueryClientProvider>
+          ),
+     });
+
+     await waitFor(() => expect(result.current[1]).toBe("success"));
+
+     const [breedList] = result.current;
+     expect(breedList).toEqual(breeds);
 });
