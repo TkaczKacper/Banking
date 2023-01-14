8b085607728f7b53fac23bf2aabe9040409fde76
TkaczKacper <kacpertk2016@gmail.com>
2023-01-15 00:29:35 +0100

    react intro completed

---
+++ frontend masters course/5-Special-case-react-tools/src/fetchSearch.jsx
@@ -0,0 +1,16 @@
+async function fetchSearch({ queryKey }) {
+     const { animal, location, breed } = queryKey[1];
+
+     const result = await fetch(
+          `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+     );
+
+     if (!result.ok) {
+          throw new Error(
+               `pet search not okay ${animal}, ${location}, ${breed}`
+          );
+     }
+     return result.json();
+}
+
+export default fetchSearch;
