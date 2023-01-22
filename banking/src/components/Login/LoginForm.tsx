e38327d2af8957641a47804179f3fe1ca882ad8d
TkaczKacper <kacpertk2016@gmail.com>
2023-01-22 13:19:33 +0100

    client side form valid

--- banking/src/components/Login/LoginForm.tsx
+++ banking/src/components/Login/LoginForm.tsx
@@ -1,15 +1,68 @@
 import React from "react";
 import './loginForm.css';
+import { Formik, FormikErrors } from "formik";
+
+interface FormValues {
+     username: string;
+     password: string;
+}
 
 const LoginForm = () => {
-
+     const initialValues: FormValues = { username: '', password: '' };
      return (
           <div className="form-container">
-               <form className="login-form" onSubmit={(e) => { e.preventDefault() }}>
-                    <input type="text" placeholder="username" />
-                    <input type="password" placeholder="password" />
-                    <button className="form-button" type="submit">Login</button>
-               </form>
+               <Formik
+                    initialValues={initialValues}
+                    validate={values => {
+                         let errors: FormikErrors<FormValues> = {}
+                         if (!values.username) {
+                              errors.username = 'Username required';
+                         }
+                         if (!values.password) {
+                              errors.password = 'Password required';
+                         }
+                         return errors;
+                    }}
+                    onSubmit={(values, { setSubmitting }) => {
+                         setTimeout(() => {
+                              alert(JSON.stringify(values, null, 2));
+                              setSubmitting(false);
+                         }, 400);
+                    }}
+               >
+                    {({
+                         values,
+                         errors,
+                         touched,
+                         handleChange,
+                         handleBlur,
+                         handleSubmit,
+                         isSubmitting,
+                    }) => (
+                         <form className="login-form" onSubmit={handleSubmit}>
+                              <input
+                                   type="username"
+                                   name="username"
+                                   placeholder="username"
+                                   onChange={handleChange}
+                                   onBlur={handleBlur}
+                                   value={values.username}
+                                   autoComplete="off"
+                              />
+                              {errors.username}
+                              <input
+                                   type="password"
+                                   name="password"
+                                   placeholder="password"
+                                   onChange={handleChange}
+                                   onBlur={handleBlur}
+                                   value={values.password}
+                              />
+                              {errors.password}
+                              <button className="form-button" type="submit" disabled={isSubmitting}>Login</button>
+                         </form>
+                    )}
+               </Formik>
           </div>
      );
 };
--- banking/src/components/Login/RegisterForm.tsx
+++ banking/src/components/Login/RegisterForm.tsx
@@ -1,17 +1,101 @@
 import React from "react";
 import './loginForm.css';
+import { Formik, FormikErrors } from "formik";
+
+
+interface FormValues {
+     username: string;
+     email: string;
+     password: string;
+     password_conf: string;
+}
 
 const RegisterForm = () => {
+     const initialValues: FormValues = { username: '', email: '', password: '', password_conf: '' };
      return (
           <>
                <div className="form-container register-form">
-                    <form className="login-form" onSubmit={(e) => { e.preventDefault() }}>
-                         <input type="text" placeholder="username" name="username" autoComplete="off" />
-                         <input type="text" placeholder="e-mail" name="e-mail" autoComplete="off" />
-                         <input type="password" placeholder="password" name="password" />
-                         <input type="password" placeholder="confirm password" name="password_conf" />
-                         <button className="form-button" type="submit">Register</button>
-                    </form>
+                    <Formik
+                         initialValues={initialValues}
+                         validate={values => {
+                              let errors: FormikErrors<FormValues> = {}
+                              if (!values.username) {
+                                   errors.username = 'Username required';
+                              }
+                              if (!values.email) {
+                                   errors.email = 'Email required';
+                              } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+                                   errors.email = 'Invalid email';
+                              }
+                              if (!values.password) {
+                                   errors.password = 'Password required';
+                              } else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/i.test(values.password)) {
+                                   errors.password = 'Weak password';
+                              }
+                              if (values.password_conf != values.password) {
+                                   errors.password_conf = 'Password must be identical'
+                              }
+                              return errors;
+                         }}
+                         onSubmit={(values, { setSubmitting }) => {
+                              setTimeout(() => {
+                                   alert(JSON.stringify(values, null, 2));
+                                   setSubmitting(false);
+                              }, 400);
+                         }}
+                    >
+                         {({
+                              values,
+                              errors,
+                              touched,
+                              handleChange,
+                              handleBlur,
+                              handleSubmit,
+                              isSubmitting,
+                         }) => (
+                              <form className="login-form" onSubmit={handleSubmit}>
+                                   <input
+                                        type="text"
+                                        placeholder="username"
+                                        name="username"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.username}
+                                        autoComplete="off"
+                                   />
+                                   {errors.username}
+                                   <input
+                                        type="text"
+                                        placeholder="e-mail"
+                                        name="email"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.email}
+                                        autoComplete="off"
+                                   />
+                                   {errors.email}
+                                   <input
+                                        type="password"
+                                        placeholder="password"
+                                        name="password"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.password}
+                                   />
+                                   {errors.password}
+                                   <input
+                                        type="password"
+                                        placeholder="confirm password"
+                                        name="password_conf"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.password_conf}
+                                   />
+                                   {errors.password_conf}
+                                   <button className="form-button" type="submit" disabled={isSubmitting}>Register</button>
+                              </form>
+                         )}
+                    </Formik>
                </div>
           </>
      );
