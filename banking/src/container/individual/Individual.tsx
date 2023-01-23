9cb31ecc2e7f4070f359a3ae2b5ecb0f3cbad69c
TkaczKacper <kacpertk2016@gmail.com>
2023-01-23 19:34:06 +0100

    individual page finish

--- banking/src/container/individual/Individual.tsx
+++ banking/src/container/individual/Individual.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Footer } from "../../components";
 import './individual.css';
+import '../../components/NavBar/navbar.css';
 
 const Individual = () => {
      return (
@@ -8,7 +9,15 @@ const Individual = () => {
                <div className="individual-page-container">
                     <div className="container">
                          <div className="page-text-container">
-
+                              <p className="text-h1">Otwórz konto, zagrnij do 450zł</p>
+                              <p className="text-normal">i oszczędzaj nawet na <strong>8%!</strong></p>
+                              <p className="text-h2">Do konta mamy dla Ciebie jeszcze:</p>
+                              <ul className="text-ul">
+                                   <li>dodatkową <strong>eKartę</strong> do zakupów w internecie</li>
+                                   <li><strong>premię</strong> za polecenie konta znajomym</li>
+                              </ul>
+                              <button className="register-btn" id="register-individual">załóż konto przez aplikację</button>
+                              <p className="text-link">przeczytaj więcej</p>
                          </div>
                          <div className="page-image-container">
                               <img className="page-image" src="https://www.mbank.pl/grafiki/ilustracje/indywidualny/retail_card_zero_cost-1.svg??__scale=h:74,w:200" />
