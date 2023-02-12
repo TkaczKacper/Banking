5a10332584db02f3c862add002438b92e47b1ccc
TkaczKacper <kacpertk2016@gmail.com>
2023-02-13 00:49:27 +0100

    convert to pln

--- banking/packages/client/src/components/CurrencyExchange/CurrencyExchange.tsx
+++ banking/packages/client/src/components/CurrencyExchange/CurrencyExchange.tsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from "react";
 
 const CurrencyExchange = () => {
    const [data, setData] = useState([]);
+   const [moneyAmount, setMoneyAmount] = useState(0);
+   const [currency, setCurrency] = useState(0);
+   let conversionResult: number = 0;
    const fetchData = async () => {
       await fetch("https://api.exchangerate.host/latest?base=pln")
          .then((res) => res.json())
@@ -15,17 +18,44 @@ const CurrencyExchange = () => {
       fetchData();
    }, []);
 
+   function showResult() {
+      const possibleValue = Object.values(data).at(currency);
+      if (possibleValue) conversionResult = moneyAmount / possibleValue;
+      let container = document.getElementById("result") as HTMLDivElement;
+      container.innerHTML = conversionResult.toFixed(2) + "PLN";
+   }
+
    return (
       <>
          currency
          <div>XD?</div>
          <div className="cantor-container">
-            <select>
-               {Object.keys(data).map((keyName: any, index) => {
-                  return <option value={keyName}>{keyName}</option>;
-               })}
-            </select>
+            <form
+               onSubmit={(e) => {
+                  e.preventDefault();
+                  showResult();
+               }}
+            >
+               <input
+                  type="number"
+                  name="amountInput"
+                  onChange={(e) => {
+                     setMoneyAmount(Number(e.target.value));
+                  }}
+               ></input>
+               <select
+                  name="currency"
+                  onChange={(e) => {
+                     setCurrency(Number(e.target.value));
+                  }}
+               >
+                  {Object.keys(data).map((keyName: any, index) => {
+                     return <option value={index}>{keyName}</option>;
+                  })}
+               </select>
+            </form>
          </div>
+         <div id="result"></div>
       </>
    );
 };
