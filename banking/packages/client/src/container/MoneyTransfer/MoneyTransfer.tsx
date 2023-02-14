fec143bc954c3f670fb6763e2c0e30a02614f61a
TkaczKacper <kacpertk2016@gmail.com>
2023-02-14 22:47:45 +0100

    transfer front 1

--- banking/packages/client/src/container/MoneyTransfer/MoneyTransfer.tsx
+++ banking/packages/client/src/container/MoneyTransfer/MoneyTransfer.tsx
@@ -1,11 +1,80 @@
 import { AccountNavBar } from "../../components";
 import "./moneyTransfer.css";
+import { useCookies } from "react-cookie";
+import { useState, useEffect, Dispatch } from "react";
 
 const MoneyTransfer = () => {
+   const [cookie] = useCookies(["userId"]);
+   const [accounts, setAccounts] = useState([]);
+
+   const fetchData = async () => {
+      return await fetch(`http://192.168.1.100:5000/account/${cookie.userId}`, {
+         method: "GET",
+         credentials: "include",
+         headers: {
+            "Content-Type": "application/json",
+         },
+      })
+         .catch((err) => {
+            console.log(err);
+         })
+         .then((result) => {
+            if (!result || !result.ok || result.status >= 400) {
+               throw new Error("Something went wrong, try again later.");
+            }
+            return result.json();
+         })
+         .then((data) => {
+            return setAccounts(data.accounts);
+         });
+   };
+
+   useEffect(() => {
+      fetchData();
+   }, []);
+
    return (
       <>
          <AccountNavBar />
-         <div>MoneyTransfer</div>
+         <div>
+            <form
+               onSubmit={(e: React.SyntheticEvent) => {
+                  e.preventDefault();
+                  const target = e.target as typeof e.target & {
+                     sender: { value: number };
+                     amount: { value: number };
+                     receiver: { value: number };
+                  };
+                  const senderAccount = target.sender.value;
+                  const amount = target.amount.value;
+                  const receiverAccount = target.receiver.value;
+               }}
+            >
+               Waluta
+               <select name="sender">
+                  {accounts.map((item: any) => {
+                     return (
+                        <option
+                           key={item.accountnumber}
+                           value={item.accountnumber}
+                        >
+                           {item.currency}
+                        </option>
+                     );
+                  })}
+               </select>
+               Kwota
+               <input
+                  name="amount"
+                  type="number"
+                  min="0.01"
+                  step="0.01"
+               ></input>
+               Odbiorca
+               <input name="receiver" type="number" />
+               <button type="submit">submit</button>
+            </form>
+         </div>
       </>
    );
 };
