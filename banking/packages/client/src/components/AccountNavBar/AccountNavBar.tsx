d7610e92e7b432494688671458cd58c710619697
TkaczKacper <kacpertk2016@gmail.com>
2023-02-21 15:18:39 +0100

    acc nav bar

--- banking/packages/client/src/components/AccountNavBar/AccountNavBar.tsx
+++ banking/packages/client/src/components/AccountNavBar/AccountNavBar.tsx
@@ -5,14 +5,50 @@ import NewAccountModal from "../NewAccountModal/NewAccountModal";
 
 const AccountNavBar = () => {
    const [modalActive, setModalActive] = useState(false);
+   const [open, setOpen] = useState(["show", "hide"]);
+   const openMenu = () => {
+      const divsShown = document.getElementById("show") as HTMLDivElement;
+      const divsHiden = document.getElementById("hide") as HTMLDivElement;
+      const navLinks = document.getElementById(
+         "nav-links-container"
+      ) as HTMLDivElement;
+      divsShown.id = "hide";
+      divsHiden.id = "show";
+      navLinks.style.display == ""
+         ? (navLinks.style.display = "flex")
+         : (navLinks.style.display = "");
+   };
    return (
       <>
          <div className="accountNavBar">
-            <Link to={"/account"}>Twoje sprawy</Link>
-            <Link to={"/account/transfer"}>Przelew</Link>
-            <Link to={"/account/history"}>Historia transakcji</Link>
-            <Link to={"/account/exchange"}>Kantor</Link>
-            <button onClick={() => setModalActive(true)}>Nowy rachunek</button>
+            <div className="account-nav-menu" onClick={openMenu}>
+               <div className="nav-menu-opener" id={open[0]}>
+                  ⋁ twoje sprawy ⋁
+               </div>
+               <div className="nav-menu-opener" id={open[1]}>
+                  ⋀ ⋀ ⋀ ⋀ ⋀ ⋀
+               </div>
+            </div>
+            <div id="nav-links-container">
+               <Link to={"/account"} className="account-nav-link">
+                  Twoje konta
+               </Link>
+               <Link to={"/account/transfer"} className="account-nav-link">
+                  Przelew
+               </Link>
+               <Link to={"/account/history"} className="account-nav-link">
+                  Historia transakcji
+               </Link>
+               <Link to={"/account/exchange"} className="account-nav-link">
+                  Kantor
+               </Link>
+               <a
+                  className="account-nav-link"
+                  onClick={() => setModalActive(true)}
+               >
+                  Nowy rachunek
+               </a>
+            </div>
          </div>
          <NewAccountModal
             modalActive={modalActive}
